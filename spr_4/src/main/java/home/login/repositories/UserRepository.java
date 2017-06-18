fedad2282e15d581ad396d2d96d615e496e212de
snekky <kordevg@yandex.ru>
2017-06-18 13:34:02 +0500

    Update UserRepository.java

--- spr_4/src/main/java/home/login/repositories/UserRepository.java
+++ spr_4/src/main/java/home/login/repositories/UserRepository.java
@@ -3,16 +3,16 @@ package home.login.repositories;
 import home.login.entities.User;
 
 
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
+
 
 
 
 @Component
-public interface UserRepository extends Repository {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
 
     User findByUsername(String username);
-
-    void save(User user);
 
 }
